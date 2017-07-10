ae6373bf4fec5a0ac26a602f4cf67f6a39938cbf
Qasim Hasnain Baqri <mqhasnain@tekion.com>
2017-07-10 16:24:28 +0530

    initial commit after creating new apis based on  new dealer screen

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"bitbucket.org/tekion/tdealer/tdealerService"
+	"bitbucket.org/tekion/tdealer/dealerService"
 )
 
 func main() {
-	tdealerService.Start()
+	dealerService.Start()
 }
