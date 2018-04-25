d5e05090d8bc72ba5aeb9be70d2f406fd5ed1cb9
yog prakash <yprakash@tekion.com>
2018-04-25 17:09:24 +0530

    redis cache changes

--- dealerService/common.go
+++ dealerService/common.go
@@ -2,13 +2,14 @@ package dealerService
 
 import (
 	log "bitbucket.org/tekion/tbaas/log/v1"
-	redis "bitbucket.org/tekion/tbaas/redisHelper/redisUtils"
+	redis "bitbucket.org/tekion/tbaas/redisHelper/v2"
+	enumCom "bitbucket.org/tekion/tenums/common"
 	"fmt"
 )
 
 /********* redis cache related functions ************************/
 func setCacheData(ctx *customCtx, payload interface{}, serviceID, collection string, identifier string) {
-	err := redis.SetCacheData(ctx.TContext, payload, serviceID, collection, identifier)
+	err := redis.AddOrUpdateCache(ctx.Tenant, ctx.DealerID, serviceID, collection, identifier, payload, enumCom.RedisExpireTime)
 	if err != nil {
 		err = fmt.Errorf("failed to set %s cache data in collection % s for %s , error: %v", serviceID, collection, identifier, err)
 		log.GenericError(ctx.TContext, err, nil)
