6adcaed819cb4852bee350003058f5c4a60b4147
Qasim <mqhasnain@tekion.com>
2018-04-26 20:13:42 +0530

    changed field names

--- assets/utils.go
+++ assets/utils.go
@@ -8,11 +8,11 @@ import (
 
 func (arb *assetsReqBody) findQ() (bson.M, error) {
 	findQ := bson.M{}
-	if len(arb.Source) > 0 {
-		findQ["sources"] = bson.M{"$in": arb.Source}
+	if len(arb.Origins) > 0 {
+		findQ["origins"] = bson.M{"$in": arb.Origins}
 	}
-	if len(arb.OEMs) > 0 {
-		findQ["Makes"] = bson.M{"$in": arb.OEMs}
+	if len(arb.Makes) > 0 {
+		findQ["makes"] = bson.M{"$in": arb.Makes}
 	}
 	if len(arb.DealerIDs) > 0 {
 		findQ["dealerIDs"] = bson.M{"$in": arb.DealerIDs}
