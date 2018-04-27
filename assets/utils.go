bb4bf5e5c63147409a1282e14a477c108b09f7b5
Qasim <mqhasnain@tekion.com>
2018-04-27 07:47:19 +0530

    changed field names

--- assets/utils.go
+++ assets/utils.go
@@ -3,15 +3,19 @@ package assets
 import (
 	"fmt"
 
+	"strings"
+
 	"gopkg.in/mgo.v2/bson"
 )
 
 func (arb *assetsReqBody) findQ() (bson.M, error) {
 	findQ := bson.M{}
 	if len(arb.Origins) > 0 {
+		sliceToLower(arb.Origins)
 		findQ["origins"] = bson.M{"$in": arb.Origins}
 	}
 	if len(arb.Makes) > 0 {
+		sliceToLower(arb.Makes)
 		findQ["makes"] = bson.M{"$in": arb.Makes}
 	}
 	if len(arb.DealerIDs) > 0 {
@@ -23,6 +27,12 @@ func (arb *assetsReqBody) findQ() (bson.M, error) {
 	}
 
 	return findQ, nil
+}
+
+func sliceToLower(s []string) {
+	for i, _ := range s {
+		s[i] = strings.ToLower(s[i])
+	}
 }
 
 func (arb *assetsReqBody) searchQ() bson.M {
